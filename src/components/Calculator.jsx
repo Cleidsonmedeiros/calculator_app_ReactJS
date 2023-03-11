af15e9a3b90d25dd0663d64313e35afcf0c47a71
cleidsonmedeiros <cleidsonmedeiros12@gmail.com>
2023-03-10 22:47:25 -0300

    finish

--- src/components/Calculator.jsx
+++ src/components/Calculator.jsx
@@ -4,7 +4,9 @@ import Container from '@mui/material/Container'
 import { Box } from '@mui/system'
 
 export default function Calculator() {
-    const[num, setNum]=useState(0);
+    const [num, setNum]=useState(0);
+    const [oldnum, setOldNum] = useState(0);
+    const [operator, setOperator] = useState();
 
     function inputNum(e) {
       if (num === 0) {
@@ -30,8 +32,23 @@ export default function Calculator() {
       }
     }
 
-    function calculator() {
+    function operatorHandler(e) {
+      var operatorInput = e.target.value;
+      setOperator(operatorInput);
+      setOldNum(num);
+      setNum(0);
+    }
 
+    function calculate() {
+      if (operator === "/") {
+        setNum(parseFloat(oldnum) / parseFloat(num));
+      } else if (operator === "X") {
+        setNum(parseFloat(oldnum) * parseFloat(num));
+      } else if (operator === "-") {
+        setNum(parseFloat(oldnum) - parseFloat(num));
+      } else if (operator === "+") {
+        setNum(parseFloat(oldnum) + parseFloat(num));
+      }
     }
 
   return (
@@ -45,23 +62,23 @@ export default function Calculator() {
                 <button onClick={clear}>AC</button>
                 <button onClick={changeSign}>+/-</button>
                 <button onClick={porcentage}>%</button>
-                <button id="orange">/</button>
+                <button id="orange" onClick={operatorHandler} value={"/"}>/</button>
                 <button id="gray" onClick={inputNum} value={7}>7</button>
                 <button id="gray" onClick={inputNum} value={8}>8</button>
                 <button id="gray" onClick={inputNum} value={9}>9</button>
-                <button id="orange">X</button>
+                <button id="orange" onClick={operatorHandler} value={"X"}>X</button>
                 <button id="gray" onClick={inputNum} value={4}>4</button>
                 <button id="gray" onClick={inputNum} value={5}>5</button>
                 <button id="gray" onClick={inputNum} value={6}>6</button>
-                <button id="orange">-</button>
+                <button id="orange" onClick={operatorHandler} value={"-"}>-</button>
                 <button id="gray" onClick={inputNum} value={1}>1</button>
                 <button id="gray" onClick={inputNum} value={2}>2</button>
                 <button id="gray" onClick={inputNum} value={3}>3</button>
-                <button id="orange">+</button>
+                <button id="orange" onClick={operatorHandler} value={"+"}>+</button>
                 <button id="gray" onClick={inputNum} value={0}>0</button>
                 <button id="gray" onClick={inputNum} value={"."}>.</button>
                 <button id="gray" style={{ visibility: "hidden" }}>.</button>
-                <button id="orange">=</button>
+                <button id="orange" onClick={calculate}>=</button>
             </div>
         </Container>
     </div>
